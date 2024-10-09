b7034a21febf7245a5506069b404efa78e8c7fc0
“Jayani <jayaniabhiram@gmail.com>
2024-10-09 17:00:11 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+// src/App.js
 
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Services from './components/Services/Services';
+import Contact from './components/Contact/Contact';
 
-function App() {
+
+const App = () => {
   return (
-    
-    <>
-    
-    
-    
-    
-    
-    </>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
